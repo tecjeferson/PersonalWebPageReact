8c2accb293e60d2d1d0b535789f824721f253646
Jeferson.Rocha <tecjeferson@gmail.com>
2020-02-17 23:42:37 -0300

    Deploying to Heroku

---
+++ build/precache-manifest.1f7852d71cd85117e856e5353f6ed848.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6283a47e0911e44376dd11c143833db4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7e3f32702e6909080083",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "6ff5c4cde714b4118208",
+    "url": "/static/css/main.f55fde80.chunk.css"
+  },
+  {
+    "revision": "7e3f32702e6909080083",
+    "url": "/static/js/2.b0c53420.chunk.js"
+  },
+  {
+    "revision": "3179178beb87319eed9d8dff9b274176",
+    "url": "/static/js/2.b0c53420.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ff5c4cde714b4118208",
+    "url": "/static/js/main.7bbe6980.chunk.js"
+  },
+  {
+    "revision": "fae36900f6fa58e853c1",
+    "url": "/static/js/runtime-main.cd2981ef.js"
+  },
+  {
+    "revision": "169e613642633e85b08ea817832fb1bd",
+    "url": "/static/media/891.169e6136.jpg"
+  },
+  {
+    "revision": "66607e06e3be193db85105dbc22316d1",
+    "url": "/static/media/chicago.66607e06.jpg"
+  },
+  {
+    "revision": "53774ac0a615d6f40edfdeedf494d70a",
+    "url": "/static/media/homem-olhando.53774ac0.png"
+  },
+  {
+    "revision": "73bec2e17801eaf39924d844c6956ccb",
+    "url": "/static/media/profile2.73bec2e1.jpg"
+  },
+  {
+    "revision": "9d2301afe55754a876e569b8a1e13e06",
+    "url": "/static/media/whatsapp.9d2301af.png"
+  }
+]);
